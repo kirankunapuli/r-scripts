4cfcbdfd6822a1b2b8f359ef71ff390f12afc5e1
Kiran Kunapuli V S <kirankunapuli@users.noreply.github.com>
2016-05-20 17:26:14 +0530

    Added sapply code

--- jsontrail4.R
+++ jsontrail4.R
@@ -4,9 +4,11 @@ jsfile = fromJSON("trainfull.json")
 datalength <- length(jsfile$schedules)
 
 acttypes <- {
+  
 }
 
 act <- as.data.frame(jsfile$schedules[[5]][1])
+# act <- as.data.frame(sapply(jsfile, "[", c(5)))
 
 actlength <- length(act$types)
 actlocations <- length(act$locations)
@@ -16,3 +18,14 @@ for (x in 1:actlength) {
   actlocations <-
     c(actlocations, as.data.frame(noquote(act$locations[[x]])))
 }
+
+# rbind(act$locations[[1]], act$locations[[2]], act$locations[[3]])
+
+actl1 <- act$locations[[1]][3]
+actl2 <- act$locations[[4]][3]
+
+a1 <-
+  cbind(act$locations[[1]][1], act$locations[[1]][2], actl1, act$locations[[1]][4])
+
+a4 <-
+  cbind(act$locations[[4]][1], act$locations[[4]][2], actl2, act$locations[[4]][4])
